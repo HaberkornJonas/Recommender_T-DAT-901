db243397bac3e71201a8337cf71d7de38bbcc786
Jonas Haberkorn <jonas.haberkorn16@gmail.com>
2021-01-25 22:03:49 +0100

    Adding client segementation analysis.

--- 02_code/recommender.R
+++ 02_code/recommender.R
@@ -16,6 +16,7 @@ moisDictionary <- data.frame(moisVenteId=c(1:12), moisVente=moisNoms)
 
 
 Customers <- read_csv(paste(projectPath, "03_output/Customers.csv", sep=""))
+Families <- read_csv(paste(projectPath, "03_output/Familles.csv", sep=""))
 
 
 ## GETTING CLIENT ID ###############################
@@ -39,40 +40,195 @@ repeat{
 clientDir <- paste(projectPath, "03_output/Client_", clientId, sep="")
 unlink(clientDir, recursive = TRUE)
 dir.create(file.path(clientDir), showWarnings = FALSE)
-fileConn<-file(paste(projectPath, "03_output/Client_", clientId, "/description.txt", sep=""))
+descriptionFile<-paste(projectPath, "03_output/Client_", clientId, "/description.txt", sep="")
 
 
 ## CLIENT STATISTICAL DESCRIPTION ###############################
 print("## Client statistical dedscripton")
-writeLines(c("----- STATISTICAL DESCRIPTION -----"), fileConn)
-writeLines(c(paste("ClientId:",clientId)), fileConn)
+write("----- STATISTICAL DESCRIPTION -----", file=descriptionFile, append=T)
+write(paste("ClientId:",clientId), file=descriptionFile, append=T)
 
 # Add lots of statistics (# tickets, 3 most frequented months, money spend, # products bought, etc.)
 # TODO
 
 
 
+
+
+
 ## CLIENT SEGMENTATION ANALYSIS ###############################
 print("## Client segmentation analysis")
-writeLines(c("----- SEGMENTATION ANALYSIS -----"), fileConn)
+write("----- SEGMENTATION ANALYSIS -----", file=descriptionFile, append=T)
+clientProfile <- data.frame(ClientId=clientId)
 
-# [S_R] Regularity
-# TODO
+
+
+## [S_R] Regularity
+write(paste("", sep=""), file=descriptionFile, append=T)
+write(paste("", sep=""), file=descriptionFile, append=T)
+S_R <- Customers <- read_csv(paste(projectPath, "03_output/S_R.csv", sep=""))  # Getting segmentation result
+
+# Extracting clusterId and data
+S_R.client <- subset(S_R, ClientId == clientId)                                # Extracting client entry
+mean <- S_R.client$mean                                                        # Getting client mean value
+sd <- S_R.client$sd                                                            # Getting client sd
+
+S_R.client <- S_R.client %>%                                                   
+  select(ClientId, ClusterId) %>%                                              # Keeping only ClientId and ClusterId
+  rename(S_R = ClusterId)                                                      # Renaming CluesterId column name to prepare merging
+
+clientProfile <- clientProfile %>%
+  merge(S_R.client, by="ClientId", sort=F)                                     # Merging ClusterId into the client profile
+
+# Ordering groups
+orderedGoups <- S_R %>%
+  group_by(ClusterId) %>%                                                      # Grouping segmentation data by ClusterId
+  summarise(meanDist = mean(sqrt(mean*mean+sd*sd))) %>%                        # Calculating mean distance with origin for each cluster
+  arrange(desc(meanDist))                                                      # Ordering clusters by their distance
+
+# Getting group position and data
+groupPos <- match(clientProfile$S_R, orderedGoups$ClusterId)                   # Getting user's cluster position
+group <- S_R %>%
+  filter(S_R$ClusterId==clientProfile$S_R)                                     # Keeping only data from the same cluster as the user's cluster
+maxMean <- max(group$mean)
+minMean <- min(group$mean)
+maxSD <- max(group$sd)
+minSD <- min(group$sd)
+
+# Writing segmentation analysis
+write("- Regularite (vient souvent ou non)", file=descriptionFile, append=T)
+write(paste("    Le client fait parti du groupe en position ", groupPos, " sur ", count(orderedGoups)$n, " avec une regularite moyenne de ", mean, " tickets par mois et une deviation standard de ", sd, ".", sep=""), file=descriptionFile, append=T)
+write(paste("    Le groupe possede une moyenne maximum de ", maxMean, " et minimum de ", minMean, " tickets par mois pour une deviation standard maximum de ", maxSD, " et minimum de ", minSD, ".", sep=""), file=descriptionFile, append=T)
+write(paste("    Moyenne:            [", minMean, ", ", mean, ", ", maxMean, "]", sep=""), file=descriptionFile, append=T)
+write(paste("    Deviation standard: [", minSD, ", ", sd, ", ", maxSD, "]", sep=""), file=descriptionFile, append=T)
+write(paste("", sep=""), file=descriptionFile, append=T)
+
+
+
+
 
 # [S_S_T] Spendings per tickets
-# TODO
+S_S_T <- Customers <- read_csv(paste(projectPath, "03_output/S_S_T.csv", sep=""))  # Getting segmentation result
+
+# Extracting clusterId and data
+S_S_T.client <- subset(S_S_T, ClientId == clientId)                            # Extracting client entry
+mean <- S_S_T.client$mean                                                      # Getting client mean value
+sd <- S_S_T.client$sd                                                          # Getting client sd
+
+S_S_T.client <- S_S_T.client %>%                                                   
+  select(ClientId, ClusterId) %>%                                              # Keeping only ClientId and ClusterId
+  rename(S_S_T = ClusterId)                                                    # Renaming CluesterId column name to prepare merging
+
+clientProfile <- clientProfile %>%
+  merge(S_S_T.client, by="ClientId", sort=F)                                   # Merging ClusterId into the client profile
+
+# Ordering groups
+orderedGoups <- S_S_T %>%
+  group_by(ClusterId) %>%                                                      # Grouping segmentation data by ClusterId
+  summarise(meanDist = mean(sqrt(mean*mean+sd*sd))) %>%                        # Calculating mean distance with origin for each cluster
+  arrange(desc(meanDist))                                                      # Ordering clusters by their distance
+
+# Getting group position and data
+groupPos <- match(clientProfile$S_S_T, orderedGoups$ClusterId)                 # Getting user's cluster position
+group <- S_S_T %>%
+  filter(S_S_T$ClusterId==clientProfile$S_S_T)                                 # Keeping only data from the same cluster as the user's cluster
+maxMean <- max(group$mean)
+minMean <- min(group$mean)
+maxSD <- max(group$sd)
+minSD <- min(group$sd)
+
+# Writing segmentation analysis
+write("- Depenses par produit (depenses beaucoup ou non)", file=descriptionFile, append=T)
+write(paste("    Le client fait parti du groupe en position ", groupPos, " sur ", count(orderedGoups)$n, " avec une depense par ticket moyenne de ", mean, " et une deviation standard de ", sd, ". (en euros)", sep=""), file=descriptionFile, append=T)
+write(paste("    Le groupe possede une moyenne de depenses par ticket maximum de ", maxMean, " et minimum de ", minMean, " pour une deviation standard maximum de ", maxSD, " et minimum de ", minSD, ". (en euros)", sep=""), file=descriptionFile, append=T)
+write(paste("    Moyenne:            [", minMean, ", ", mean, ", ", maxMean, "]", sep=""), file=descriptionFile, append=T)
+write(paste("    Deviation standard: [", minSD, ", ", sd, ", ", maxSD, "]", sep=""), file=descriptionFile, append=T)
+write(paste("", sep=""), file=descriptionFile, append=T)
+
+
+
+
 
 # [S_S_I] Spendings per items
-# TODO
+S_S_I <- Customers <- read_csv(paste(projectPath, "03_output/S_S_I.csv", sep=""))  # Getting segmentation result
+
+# Extracting clusterId and data
+S_S_I.client <- subset(S_S_I, ClientId == clientId)                            # Extracting client entry
+mean <- S_S_I.client$mean                                                      # Getting client mean value
+sd <- S_S_I.client$sd                                                          # Getting client sd
+
+S_S_I.client <- S_S_I.client %>%                                                   
+  select(ClientId, ClusterId) %>%                                              # Keeping only ClientId and ClusterId
+  rename(S_S_I = ClusterId)                                                    # Renaming CluesterId column name to prepare merging
+
+clientProfile <- clientProfile %>%
+  merge(S_S_I.client, by="ClientId", sort=F)                                   # Merging ClusterId into the client profile
+
+# Ordering groups
+orderedGoups <- S_S_I %>%
+  group_by(ClusterId) %>%                                                      # Grouping segmentation data by ClusterId
+  summarise(meanDist = mean(sqrt(mean*mean+sd*sd))) %>%                        # Calculating mean distance with origin for each cluster
+  arrange(desc(meanDist))                                                      # Ordering clusters by their distance
+
+# Getting group position and data
+groupPos <- match(clientProfile$S_S_I, orderedGoups$ClusterId)                 # Getting user's cluster position
+group <- S_S_I %>%
+  filter(S_S_I$ClusterId==clientProfile$S_S_I)                                 # Keeping only data from the same cluster as the user's cluster
+maxMean <- max(group$mean)
+minMean <- min(group$mean)
+maxSD <- max(group$sd)
+minSD <- min(group$sd)
+
+# Writing segmentation analysis
+write("- Depenses par produit (achete des produits chere ou non)", file=descriptionFile, append=T)
+write(paste("    Le client fait parti du groupe en position ", groupPos, " sur ", count(orderedGoups)$n, " avec une depense par produit moyenne de ", mean, " et une deviation standard de ", sd, ". (en euros)", sep=""), file=descriptionFile, append=T)
+write(paste("    Le groupe possede une moyenne de depenses par produit maximum de ", maxMean, " et minimum de ", minMean, " pour une deviation standard maximum de ", maxSD, " et minimum de ", minSD, ". (en euros)", sep=""), file=descriptionFile, append=T)
+write(paste("    Moyenne:            [", minMean, ", ", mean, ", ", maxMean, "]", sep=""), file=descriptionFile, append=T)
+write(paste("    Deviation standard: [", minSD, ", ", sd, ", ", maxSD, "]", sep=""), file=descriptionFile, append=T)
+write(paste("", sep=""), file=descriptionFile, append=T)
+
+
+
+
 
 # [S_F] Prefered product family
-# TODO
+S_F <- Customers <- read_csv(paste(projectPath, "03_output/S_F.csv", sep=""))  # Getting segmentation result
+
+# Extracting clusterId and data
+S_F.client <- subset(S_F, ClientId == clientId)                                # Extracting client entry
+clusterId <- S_F.client$ClusterId 
+famillyId <- S_F.client$FamilleId                                              # Getting client prefered product family
+n <- S_F.client$n                                                              # Getting number of bought product in the family
+
+S_F.client <- S_F.client %>%                                                   
+  select(ClientId, ClusterId) %>%                                              # Keeping only ClientId and ClusterId
+  rename(S_F = ClusterId)                                                      # Renaming CluesterId column name to prepare merging
+
+clientProfile <- clientProfile %>%
+  merge(S_F.client, by="ClientId", sort=F)                                     # Merging ClusterId into the client profile
+
+# Getting group position and data
+group <- S_F %>%
+  filter(S_F$ClusterId==clientProfile$S_F)                                     # Keeping only data from the same cluster as the user's cluster
+maxN <- max(group$n)
+minN <- min(group$n)
+famillyName <- subset(Families, FamilleId == famillyId)$Famille
+
+# Writing segmentation analysis
+write("- Famille de produits preferee", file=descriptionFile, append=T)
+write(paste("    Le client fait parti du groupe ", clusterId, " qui prefere la famille de produit ", famillyName, ", dont il a achete ", n, " produits.", sep=""), file=descriptionFile, append=T)
+write(paste("    Le groupe a achete au maximum  ", maxN, " et au minimum ", minN, " produits de la famille ", famillyName, ".", sep=""), file=descriptionFile, append=T)
+write(paste("    Produits achetes:   [", minN, ", ", n, ", ", maxN, "]", sep=""), file=descriptionFile, append=T)
+write(paste("", sep=""), file=descriptionFile, append=T)
+
+
+
 
 
 
 ## CLIENT RECOMMENDATIONS ###############################
 print("## Client recommendations")
-writeLines(c("----- CLIENT RECOMMENDATIONS -----"), fileConn)
+write("----- CLIENT RECOMMENDATIONS -----", file=descriptionFile, append=T)
 
 # [R_1] "Other profiles like yours also like..." 
 # TODO
@@ -86,4 +242,4 @@ writeLines(c("----- CLIENT RECOMMENDATIONS -----"), fileConn)
 
 
 ## CLEAN UP ###############################
-close(fileConn)
+
